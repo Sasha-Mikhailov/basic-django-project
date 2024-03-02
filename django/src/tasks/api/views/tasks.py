85eb9620e82fd4c53aee9dfda6f70fb5799bcbdb
Sasha Mikhailov <sasha.mikhailov@klarna.com>
2024-03-02 19:44:16 +0100

    tasks: add task reassign action

--- django/src/tasks/api/views/tasks.py
+++ django/src/tasks/api/views/tasks.py
@@ -1,4 +1,8 @@
+import random
+
 from rest_framework import permissions, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from tasks.models import Task, TaskCost, TaskStatus, TaskUser
 from tasks.api.serializers import TaskSerializer, TaskUserSerializer  #, TaskCostSerializer, TaskStatusSerializer
@@ -22,3 +26,24 @@ class TaskViewSet(viewsets.ModelViewSet):
     serializer_class = TaskSerializer
     # permission_classes = [permissions.IsAuthenticated]
     permission_classes = [permissions.AllowAny]
+
+    @action(detail=False, methods=['post'], url_path='reassign', url_name='reassign', permission_classes=[permissions.AllowAny])
+    def reassign_tasks(self, request):
+        """
+        get all tasks in progress and reassign them to a random worker
+        """
+        tasks_in_progress = Task.objects.filter(
+            status=Task.Status.ASSIGNED
+        )
+        if len(tasks_in_progress) == 0:
+            return Response({'status': 'No tasks with status=assigned available — nothing to reassign'})
+
+        worker_users = TaskUser.objects.filter(role='worker')
+        if len(worker_users) == 0:
+            return Response({'status': 'No users with role=worker available; can\'t reassign tasks'})
+
+        for task in tasks_in_progress:
+            task.user = worker_users[random.randint(0, len(worker_users) - 1)]
+            task.save()
+
+        return Response({'status': f'{len(tasks_in_progress)} tasks reassigned'})
