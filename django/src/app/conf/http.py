b69d5a2278c91e9b23a91072c198e42e6a334eaf
Sasha Mikhailov <sasha.mikhailov@klarna.com>
2024-03-03 12:49:55 +0100

    http: update CSRF_TRUSTED_ORIGINS

--- django/src/app/conf/http.py
+++ django/src/app/conf/http.py
@@ -2,7 +2,7 @@ from app.conf.environ import env
 
 ALLOWED_HOSTS = ["*"]  # host validation is not necessary in 2020
 CSRF_TRUSTED_ORIGINS = [
-    "http://your.app.origin",
+    "http://164.90.194.104/",
 ]
 
 if env("DEBUG"):
